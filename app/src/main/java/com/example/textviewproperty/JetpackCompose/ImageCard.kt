e5d452f20e00c5cfd0dd9bc7f813d2cb63098cbe
deveploper99 <rtxdeveploper99@gmail.com>
2026-01-11 23:42:18 +0600

    iamge learn jetpack compose

---
+++ app/src/main/java/com/example/textviewproperty/JetpackCompose/ImageCard.kt
@@ -0,0 +1,101 @@
+package com.example.textviewproperty.JetpackCompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.textviewproperty.R
+import com.example.textviewproperty.Text
+
+
+@Composable
+@Preview(showSystemUi = true)
+fun imageCard(
+//    painter: Painter,
+//    contentDescription: String,
+//    title:String,
+//    modifier: Modifier = Modifier
+
+    ){
+    Card(
+        modifier = Modifier.fillMaxWidth()
+            .padding(20.dp),
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(
+//                        Brush.verticalGradient(
+//                            colors = listOf(
+//                                Color.Transparent,
+//                                Color.Black
+//                            ),
+//                            startY = 300f
+//                        )
+//                    )
+//            ) { }
+
+            Box(
+                modifier = Modifier.
+                fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+                ,
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text="Rassel Hassan You are the best",
+                    fontSize = 23.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+            }
+        }
+
+    }
+
+
+}
